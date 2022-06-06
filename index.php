518cf39cb6fe91f30001ab430709ee8310cbf296
pauline <e2194480@cmaisonneuve.qc.ca>
2022-06-06 17:05:10 -0400

    s4 c6 index.php résumé paragraphe et liens

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
 
 <?php
     if(have_posts()) :
-        while(have_posts()) : the_post() ?>
-            <p>
-            <?php the_title(); ?>
-            </p>
+        while(have_posts()) : the_post(); ?>
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+
+            <?php $lien = "<a href='" . get_permalink() . "'>lien</a>"; 
+            echo $lien;
+            ?>
+            <p><?= wp_trim_words(get_the_content(), 20, "") ; ?></p>
         <?php endwhile; ?>
 
     <?php endif; ?>
