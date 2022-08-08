fa3260be96fdadee929fd0c1ef86a85d4520b49d
pauline <e2194480@cmaisonneuve.qc.ca>
2022-08-08 14:47:30 -0400

    modification front-page.php

--- front-page.php
+++ front-page.php
@@ -7,16 +7,6 @@
             <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
             <h3><?php the_field('sous_titre'); ?></h3>
             <p><?php the_field('resume'); ?></p>
-            <?php $lien = " ... <a class='bouton bouton-colored' href='" 
-                    . get_permalink()  
-                    . "'>"
-                    . substr(get_the_title(),0,8) 
-                    ."</a>"; 
-
-    ?>
-
-    <p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
-
 
         <?php endwhile; ?>
     <?php endif; ?>    
