356af218404d08195983073799fac80c3816f9f1
pauline <e2194480@cmaisonneuve.qc.ca>
2022-08-08 14:43:51 -0400

    modification h2 front-page.php

--- front-page.php
+++ front-page.php
@@ -4,7 +4,8 @@
     <?php
     if (have_posts()):
         while(have_posts()) : the_post(); ?>
-            <h2><?php the_title(); ?></h2>
+            <h2><?php the_field('titre'); ?></h2>
+            <h3><?php the_field('sous_titre'); ?></h3>
             <?php $lien = " ... <a class='bouton bouton-colored' href='" 
                     . get_permalink()  
                     . "'>"
